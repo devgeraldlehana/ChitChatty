0da9cb4a08c5f838dbe93f5f91bb96b6eb2e7349
Gerald Lehana <glehana@tracker.co.za>
2018-03-19 21:14:59 +0200

    Network controller

---
+++ ChitChatty/Controller/NetworkController.swift
@@ -0,0 +1,73 @@
+//
+//  NetworkController.swift
+//  ChitChatty
+//
+//  Created by Gerald Lehana on 2018/03/19.
+//  Copyright © 2018 Gerald Lehana. All rights reserved.
+//
+
+import UIKit
+
+class NetworkController {
+    class func login(withUsername username:String, andPassword password:String, completion: @escaping (LoginInfo?) -> Void) {
+        let body = ["username" : username, "password" : password]
+        let session = URLSession(configuration: .ephemeral, delegate: nil, delegateQueue: OperationQueue.main)
+        var request = URLRequest(url: URL(string: "http://mobileexam.dstv.com/login")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: body, options: []) else { return }
+        request.httpBody = httpBody
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            guard let data = data else { return }
+            do {
+                let loginInfo = try JSONDecoder().decode(LoginInfo.self, from: data)
+                completion(loginInfo)
+                print("response data:", loginInfo)
+            } catch let err {
+                print("Login Error: \(err.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
+    
+    class func retrieveFriends(withGUID guid:String, andLastname lastname:String, completion: @escaping (FriendsInfo?) -> Void) {
+        let session = URLSession(configuration: .ephemeral, delegate: nil, delegateQueue: OperationQueue.main)
+        var request = URLRequest(url: URL(string: "http://mobileexam.dstv.com/friends;uniqueID=\(guid);name=\(lastname)")!)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            guard let data = data else { return }
+            do {
+                let friendsinfo = try JSONDecoder().decode(FriendsInfo.self, from: data)
+                completion(friendsinfo)
+                print("response data:", friendsinfo)
+            } catch let err {
+                print("Login Error: \(err.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
+    
+    class func image(fromUrl url:String, completion: @escaping (UIImage?) -> Void) {
+        let session = URLSession(configuration: .ephemeral, delegate: nil, delegateQueue: OperationQueue.main)
+        var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            guard let data = data else { return }
+            guard let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200 else { return }
+            let image = UIImage(data: data)
+            completion(image)
+        }
+        task.resume()
+    }
+}
