19331a820e353005b1705102d5a8491d74a4b58a
Gerald Lehana <glehana@tracker.co.za>
2018-03-20 04:40:31 +0200

    linked and set up friend details, UI is a bit shaky🤮

--- ChitChatty/Controller/FriendsDetailsViewController.swift
+++ ChitChatty/Controller/FriendsDetailsViewController.swift
@@ -10,4 +10,35 @@ import UIKit
 
 class FriendsDetailsViewController: BaseViewController {
     
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var fullname: UILabel!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var dob: UILabel!
+    @IBOutlet weak var statusImg: UIImageView!
+    @IBOutlet weak var statusTxt: UILabel!
+    
+    var friend:Friends?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let frnd = friend else {
+            presentDialog(message: "Unexpected error.")
+            return
+        }
+        self.title = frnd.alias
+        NetworkController.image(fromUrl: frnd.imageURL, completion: { image in
+            guard let img = image?.resizeImage(targetSize: CGSize(width: 100, height: 100)) else { return }
+            self.image.image = img
+        })
+        
+        fullname.text = "\(frnd.firstName) \(frnd.lastName)"
+        username.text = frnd.alias
+        dob.text = frnd.dateOfBirth
+        if frnd.status.elementsEqual("Busy") {
+            statusImg.image = UIImage(named: "busy")
+            statusTxt.text = frnd.status
+        } else if frnd.status.elementsEqual("Away") {
+            statusImg.image = UIImage(named: "away")
+            statusTxt.text = frnd.status
+        }
+    }
 }
